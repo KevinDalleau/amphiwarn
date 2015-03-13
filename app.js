d3240db330984397389f6b9ea205f8b43cbc81cc
Kevin Dalleau <kevin.dalleau@telecomnancy.eu>
2015-03-13 21:33:59 +0100

    The emitter doesn't get any notification

--- app.js
+++ app.js
@@ -1,63 +1,67 @@
-var app = angular.module("webAlert", []);
-var array = new Array();
-app.controller('MainController', function($scope, $window){
+	var app = angular.module("webAlert", []);
+	var array = new Array();
+	app.controller('MainController', function($scope, $window){
 
-	$scope.message = "Ok !";
-	$scope.watch = 
-	$scope.notifications = array;
-});
+		$scope.message = "Ok !";
+		$scope.watch = 
+		$scope.notifications = array;
+	});
 
 
-function getTimeMessage() {
-	var time = new Date();
-	return time.getHours()+":"+time.getMinutes()+":"+time.getSeconds();
-}
-
-function notifyAll() {
-	// console.log("Notification");
-	socket.emit('notify', {type:'Notification',date:getTimeMessage()});
-};
-window.addEventListener("keydown", checkKeyPressed, false);
- 
-function checkKeyPressed(e) {
-    if (e.keyCode == "13" || e.keycode == "18") {
-        notifyAll();
-    }
-}
-function notify(message) {
-// Voyons si le navigateur supporte les notifications
-	var appElement = document.querySelector('[ng-app=webAlert]');
-    var $scope = angular.element($('body')).scope();
-    
-	if(array.length<=4) {
-		array.unshift(message);
+	function getTimeMessage() {
+		var time = new Date();
+		return time.getHours()+":"+time.getMinutes()+":"+time.getSeconds();
 	}
-	else {
-		array[0] = message;
+
+	function notifyAll() {
+		// console.log("Notification");
+		socket.emit('notify', {type:'Notification',date:getTimeMessage(),sender:socket.id});
+	};
+	window.addEventListener("keydown", checkKeyPressed, false);
+	 
+	function checkKeyPressed(e) {
+	    if (e.keyCode == "13" || e.keycode == "18") {
+	        notifyAll();
+	    }
 	}
-	$scope.$apply(function() {
-        $scope.notifications = array;
-    });
-  if (!("Notification" in window)) {
-    alert("Ce navigateur ne supporte pas les notifications desktop");
-  }
+	function notify(message) {
+	// Voyons si le navigateur supporte les notifications
+		var appElement = document.querySelector('[ng-app=webAlert]');
+	    var $scope = angular.element($('body')).scope();
+	    
+		if(array.length<=4) {
+			array.unshift(message);
+		}
+		else {
+			array[0] = message;
+		}
+		$scope.$apply(function() {
+	        $scope.notifications = array;
+	    });
+	    if(message.sender!=socket.id) { //If it's not the sender
+	    	if (!("Notification" in window)) {
+	    alert("Ce navigateur ne supporte pas les notifications desktop");
+	  }
 
-  // Voyons si l'utilisateur est OK pour recevoir des notifications
-  else if (Notification.permission === "granted") {
-    // Si c'est ok, créons une notification
-    var notification = new Notification(message.date+ " : "+message.message);
-  }
+	  // Voyons si l'utilisateur est OK pour recevoir des notifications
+	  else if (Notification.permission === "granted") {
+	    // Si c'est ok, créons une notification
+	    var notification = new Notification(message.date+ " : "+message.message+' : '+message.sender+" ; "+message.id);
+	  }
 
-  
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
+	  
+	  else if (Notification.permission !== 'denied') {
+	    Notification.requestPermission(function (permission) {
 
-      if(!('permission' in Notification)) {
-        Notification.permission = permission;
-      }
-    });
-  }
+	      if(!('permission' in Notification)) {
+	        Notification.permission = permission;
+	      }
+	    });
+	  }
+	    }
+	    
+	  
 
-  // Comme ça, si l'utlisateur a refusé toute notification, et que vous respectez ce choix,
-  // il n'y a pas besoin de l'ennuyer à nouveau.
-}
+	  // Comme ça, si l'utlisateur a refusé toute notification, et que vous respectez ce choix,
+	  // il n'y a pas besoin de l'ennuyer à nouveau.
+	}
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var io = require('socket.io').listen(server);
 
 	app.use(express.static(__dirname +'/'));
 	app.use(express.static(__dirname +'/bower_components/angular/'));
-	
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -15,7 +15,7 @@ io.sockets.on('connection', function(socket){
   console.log('a user connected');
   socket.on('notify', function(data) {
 	console.log('Notification sent');
-	socket.broadcast.emit('notificationbroadcast',{message: 'Notez !', date: data.date});
+		io.sockets.emit('notificationbroadcast',{message: 'Notez !', date: data.date, sender: data.sender});
 });	
 });
 io.on('disconnect', function() {
